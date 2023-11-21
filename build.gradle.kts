0c85159d1a8bf9a400fb9e3f6171165368a1ad1f
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-11-21 08:41:38 -0500

    Agregando dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://jitpack.io")
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
     implementation("org.jetbrains:annotations:24.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0-rc1");
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0-rc1")
+    implementation("com.github.MrKloan:result-type:1.1.0")
+    implementation("io.vavr:vavr:0.10.4")
 
 }
 
