a4aef748a6a7dd498e5b6cae375252fbfdba24a2
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-11-21 18:37:16 -0500

    Implementar Deserialización y Serialiación + Tests

--- src/main/java/com/utp/ac/antonio_ng/examen/pktCaja/pktProducto/clsArticulo.java
+++ src/main/java/com/utp/ac/antonio_ng/examen/pktCaja/pktProducto/clsArticulo.java
@@ -4,19 +4,58 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.utp.ac.antonio_ng.examen.pktCaja.clsCliente;
+import com.utp.ac.antonio_ng.examen.pktCaja.clsFactura;
 import com.utp.ac.antonio_ng.examen.pktInventario.clsLoadedInventario;
+import com.utp.ac.antonio_ng.examen.pktResourceFiles.FileResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class clsArticulo {
     public static void main(String[] args) {
-
+        generar_datos();
     }
 
-    @JsonProperty("codigo")
+    public static List<clsArticulo> generar_datos() {
+        // Leer y deserializar los datos del archivo
+        List<clsArticulo> lista_articulos = List.of();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            File input_stream = new FileResource().getFileFromResource("inventario.json");
+            lista_articulos = Arrays.asList(mapper.treeToValue(mapper.readTree(input_stream).get("data"), clsArticulo[].class));
+        } catch (Exception e) {
+            System.out.println("Error leyendo los datos de \"inventario.json\".");
+            System.exit(1);
+        }
+        lista_articulos.forEach(System.out::println);
+        System.out.println("\nFin de la deserialización.....\n");
+
+        return lista_articulos;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        try {
+            ret = new ObjectMapper().writeValueAsString(this);
+        } catch (Exception e) {
+            System.out.println("Falla en deserializando el objeto:\n" + e);
+        }
+        return ret;
+    }
+
+    @JsonProperty("CODIGO")
     String codigo;
+    @JsonProperty("DESCRIPCION")
     String descripcion;
-    @JsonProperty("impuesto")
+    @JsonProperty("IMPUESTO")
     int itbms;
     @JsonIgnore
     public boolean EXENTO;
@@ -46,9 +85,11 @@ public class clsArticulo {
         return ret;
     }
 
-    @JsonProperty("existencia")
+    @JsonProperty("CANTIDAD EXISTENTE")
     int cantidad;
+    @JsonProperty("PRECIO UNITARIO")
     float costo;
+    @JsonProperty("PRECIO DE VENTA")
     float precio;
 
 
