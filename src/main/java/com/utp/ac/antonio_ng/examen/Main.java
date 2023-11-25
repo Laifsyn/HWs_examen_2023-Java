b9073252e69643be6d7454868116be41eeb0e0de
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-11-25 11:39:12 -0500

    Incorporamos Inicio Sesion con Facturador

--- src/main/java/com/utp/ac/antonio_ng/examen/Main.java
+++ src/main/java/com/utp/ac/antonio_ng/examen/Main.java
@@ -1,13 +1,27 @@
 package com.utp.ac.antonio_ng.examen;
 
+import com.utp.ac.antonio_ng.examen.pktCaja.pktProducto.clsCobrar;
+import com.utp.ac.antonio_ng.examen.pktInventario.clsLoadedInventario;
+import com.utp.ac.antonio_ng.examen.pktPasswordAntonioNg.clsPasswordAntonioNg;
+
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
-        new Main();
+        Main entry = new Main();
     }
 
     public Main() {
-
+        if (!clsPasswordAntonioNg.esperar_inicio_sesion()) {
+            JOptionPane.showMessageDialog(null, "Inicio de sesión fallada. El programa se cerrará");
+            System.exit(0);
+        }
+        System.out.println("Hola. Acabamos de leer. Inicio de sesión existosa");
+        clsCobrar.facturar_al_cliente(new clsLoadedInventario());
+        JOptionPane.showMessageDialog(null, "Finalizamos el programa");
+        System.out.println("Acabamos con la ejecución del programa.");
+        System.exit(0);
     }
 
 
